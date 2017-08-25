b11bd8a721bfdd4637a94b47a0e0ba77348814a6
Antquanique Dancy <antquaniquedancy@gmail.com>
2017-08-25 15:38:35 -0400

    Added a template that shows each individual blog.

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
-import jinja2
 
 app = Flask(__name__)
 app.config['DEBUG']=True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog4life@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] =True
 db = SQLAlchemy(app)
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
 
 class Blog(db.Model):
 
@@ -27,31 +23,37 @@ class Blog(db.Model):
 def blog():
     '''loads page with all of the blog posts'''
     blogs = Blog.query.all()
-    template = jinja_env.get_template('blog.html')
-    return template.render(blogs=blogs, title='Blog')
+    return render_template('blog.html', blogs=blogs, title='Blog')
 
 
 
 @app.route('/newpost')
 def newpost():
     '''loads page to submit a new blog'''
-    template = jinja_env.get_template('newpost.html')
-    return template.render()
+    return render_template('newpost.html')
 
 @app.route('/valid', methods=['POST'])
 def valid():
     '''checks if the blog entry is valid'''
     title = request.form['title']
     post = request.form['post']
+    
 
     if title == "" or post == "":
-        template = jinja_env.get_template('newpost.html')
-        return template.render()
+        return render_template('newpost.html')
     else:
         new_blog = Blog(title,post)
         db.session.add(new_blog)         
         db.session.commit()
-        return redirect('/blog')
+    blog_post = Blog.query.filter_by(title=title).first()
+    post_id = blog_post.id
+    return redirect('/individual?post_id='+str(post_id))
+
+@app.route('/individual')
+def individual():
+    blog_id=int(request.args.get('post_id'))
+    blog=Blog.query.get(blog_id)
+    return render_template('individual.html', blog=blog)
 
 if __name__== '__main__':
     app.run()
